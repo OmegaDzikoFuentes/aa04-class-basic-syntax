ba26b7b3705eec4e1abf12aacdc738d45110a94a
Omega Dziko F. <megaoy@yahoo.com>
2024-04-25 19:13:20 -0400

    4 otta 7

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,43 @@
-// Your code here 
+// Your code here
+
+class Person {
+
+  constructor(firstName, lastName, age) {
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+
+  }
+
+  introduce() {
+
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName} and I'm ${this.age} years old.`);
+
+  }
+
+  static introducePeople(...people) {
+
+    if(!Array.isArray(people)) {
+
+      return Error("introducePeople only takes an array as an argument.");
+
+    }
+
+    let arePeople = people.every(aPerson => aPerson instanceof Person);
+
+    if(!arePeople) {
+
+      throw new Error("All items in array must be Person class instances.")
+    } else {
+
+    introduce(arePeople);
+
+    };
+
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
